0013d9acc202bcff7359dfb542435b7eeecdcb46
Lucas Dahl <LucasDahl425@yahoo.com>
2020-08-17 09:59:45 -0700

    Added some notes on graphics.

---
+++ Notes/src/MyFirstPicture.java
@@ -0,0 +1,79 @@
+import java.awt.*; // For all graphics
+import java.util.*;
+
+public class MyFirstPicture {
+
+    public static void main(String[] args) {
+
+        DrawingPanel myPanel = new DrawingPanel();
+
+        // We draw on the graphics, not the panel
+        Graphics g = myPanel.getGraphics(); // Ask myPanel, for access to it's panel field and keep ahold of it.
+
+//        g.fillRect(10,320,50,100); //(x1, y1 are the upper left corner. dx, dy are the amount to move.
+//        g.fillRect(460,70,50,100);
+//
+//
+//
+//        g.drawLine(0,25,500,25);
+//
+//        g.drawString("Score",20,20);
+//
+//        // Part of awt
+//        Color myColor1 = new Color(200,20,0);
+//        Color myColor2 = new Color(0,230,170);
+//
+//        g.setColor(myColor1);
+//
+//        // Ball
+//        g.fillOval(200,30,20,20);
+
+        Random r = new Random();
+        for(int i = 0; i < 100; i = i +10) {
+
+            int red = r.nextInt(256);
+            int blue = r.nextInt(256);
+            int green = r.nextInt(256);
+
+            int x = r.nextInt(450);
+            int y = r.nextInt(450);
+            int dx = r.nextInt(40) + 10;
+            int dy = r.nextInt(40) + 10;
+
+            Color temp = new Color(red, blue, green);
+            g.setColor(temp);
+
+            //g.fillRect(r.nextInt(450), r.nextInt(), 50, 50);
+            //g.fillOval(r.nextInt(450), r.nextInt(450),50, 50);
+            //g.fillOval(r.nextInt(450), r.nextInt(450), r.nextInt(40) + 10, r.nextInt(40) + 10);
+            g.fillOval(x,y, dx, dy);
+
+            g.setColor(Color.WHITE);
+            g.drawOval(x, y, dx, dy);
+
+        }
+        
+        // Set back to black
+        g.setColor(Color.BLACK);
+
+        // Make the polygon
+        Polygon myPoly = new Polygon();
+
+        // Set the points for the polygon
+        myPoly.addPoint(10, 90);
+        myPoly.addPoint(50, 10);
+        myPoly.addPoint(90, 90);
+        myPoly.addPoint(50, 170);
+
+
+        // Draw the polygon
+        //g.drawPolygon(myPoly);
+
+        // Fill the polygon
+        g.fillPolygon(myPoly);
+
+
+
+    }
+
+}
